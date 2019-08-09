2a8fd33880f70cb90986436370be9c068a918ed6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-09 12:49:16 +0000

    Deploy luigimannoni/experiments to github.com/luigimannoni/experiments.git:gh-pages

---
+++ precache-manifest.0e7023d55e2ee8960a3f41f6aac490c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d156e7312c9d0c5af3b86e7494f95da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "397835daa8c3a189a5c0",
+    "url": "/static/css/main.55a005fb.chunk.css"
+  },
+  {
+    "revision": "eceb604e38a0dd2f1a44",
+    "url": "/static/js/2.e963ab57.chunk.js"
+  },
+  {
+    "revision": "397835daa8c3a189a5c0",
+    "url": "/static/js/main.21108b4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
